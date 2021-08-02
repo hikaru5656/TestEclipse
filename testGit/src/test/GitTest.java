f0b4bd3b26da2a10178ddab75f05ffe7c2163de5
hikaru5656 <higelt@yahoo.co.jp>
2021-08-02 20:36:30 +0900

    追加メッセージ

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -3,7 +3,8 @@ package test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");	
+		System.out.println("Hello GitHub!");
+		System.out.println("GitHubに接続成功！");
 	}
 
 }
